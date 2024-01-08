3691ef87d73aee52d7096583bd9c789719db901b
Hung <giahungphanngoc2002@gmail.com>
2024-01-08 08:40:32 +0700

    Express Router

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const port = 3000;
 const morgan = require('morgan');
 const app = express();
 const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
+
+app.use('/dishes', dishRouter);
 
 
-app.use(morgan('dev'));
+/*app.use(morgan('dev'));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -55,7 +58,7 @@ app.put('/dishes/:dishId', (req, res, next) => {
 app.delete('/dishes/:dishId', (req, res, next) => {
 	res.end('Deleting dish: ' + req.params.dishId);
 });
-
+*/
 
 
 const server = http.createServer(app);
@@ -63,3 +66,5 @@ const server = http.createServer(app);
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
